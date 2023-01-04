b12d7b2328de29e27d7995ddc2affd01f7450b36
pratyushsingha <pratyushsingha83@gmail.com>
2023-01-05 00:18:56 +0530

    yoo

---
+++ javaProject/src/hackerRank/OutputFormatting.java
@@ -0,0 +1,20 @@
+package hackerRank;
+import java.util.Scanner;
+import 
+
+public class OutputFormatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     Scanner sc=new Scanner(System.in);
+         System.out.println("================================");
+         for(int i=0;i<3;i++){
+             String s1=sc.next();
+             int x=sc.nextInt();
+             //Complete this line         
+         }
+         System.out.printf("%-15s%03d\\n", s1,x); 
+         System.out.println("================================");
+	}
+
+}
